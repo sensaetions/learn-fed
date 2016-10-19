0cb4df838bbb078f7fce61a8f4d1a1debd4e93e3
Sae Vang <sae.vang@collegiseducation.com>
2016-10-18 21:47:30 -0500

    add container div to homepage body markup

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php include_once('includes/globals/header.php'); ?>
 
     <main class="content-main" role="main">
-        <h1>Learn <span>F</span>ront <span>E</span>nd <span>D</span>evelopment</h1>
-        <h2>This is a test</h2>
-        <pre><code class="css">
-        <span class="hljs-keyword">.this-is-a-class</span> {
-            <span class="hljs-attribute">property</span>: value;
-        }
-    </code></pre></main>
+        <div class="container">
+            <h1>Learn <span>F</span>ront <span>E</span>nd <span>D</span>evelopment</h1>
+            <h2>This is a test</h2>
+<pre><code class="css"><span class="hljs-keyword">.this-is-a-class</span> {
+    <span class="hljs-attribute">property</span>: value;
+}</code></pre>
+
+        </div>
+    </main>
     <?php include_once('includes/globals/footer.php'); ?>
